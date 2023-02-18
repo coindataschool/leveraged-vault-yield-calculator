62b9ee91b9a57b81c97427d634c5b8a9cef96bdd
Coin Data School <coindataschool@gmail.com>
2023-02-18 11:14:39 +0900

    break received yield into different components

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ c1, c2 = st.columns(2)
 with c1:
     capital = st.number_input('Collateral ($)', min_value=100, value=10_000)
     borrow_mult = st.number_input('Borrow Multiple', min_value=1, value=5)
-    dur = st.number_input('Duration (month)', min_value=1, value=1)
-        
-with c2:
-    rate_get = st.number_input('Supply Yield (APR, %)', min_value=5, value=10)
     rate_pay = st.number_input('Borrow Rate (APR, %)', min_value=1, value=7)
-
+    dur = st.number_input('Duration (month)', min_value=1, value=1)
+with c2:
+    stETH_yield = st.number_input('stETH Yield (APR, %)', min_value=1, value=10)
+    swap_fees = st.number_input('Balancer wstETH/WETH Pool Swap Fees (APR, %)', min_value=0.1, value=0.5)
+    bal_rewards = st.number_input('Bal Incentives (APR, %)', min_value=0, value=5)
+    lido_rewards = st.number_input('Lido Incentives (APR, %)', min_value=0, value=0.5)
+    aura_rewards = st.number_input('Aura Incentives (APR, %)', min_value=0, value=12.5) 
+    
 # calculate effective yield
 borrow = capital * borrow_mult
 deposit = capital + borrow
-monthly_rate_get = rate_get/100/12
+monthly_rate_get = (stETH_yield+swap_fees+bal_rewards+lido_rewards+aura_rewards)/100/12
 monthly_rate_pay = rate_pay/100/12
 earnings = deposit * (1 + monthly_rate_get*dur) - borrow * (1 + monthly_rate_pay*dur)
 effective_yield = (earnings / capital - 1) / dur * 12
